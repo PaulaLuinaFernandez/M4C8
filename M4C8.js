2ff82a6b4b60e9bf377298076591850a3d7ce0ed
PaulaLuinaFernandez <152091955+PaulaLuinaFernandez@users.noreply.github.com>
2024-05-16 12:02:15 +0200

    Add files via upload

---
+++ M4C8.js
@@ -0,0 +1,16 @@
+// Ejercicio 1
+const miLista = ["velma", "exploradora", "jane", "john", "harry"];
+for (var i = 0; i < miLista.length; i++) {
+   console.log(miLista[i]);
+}
+
+// Ejercicio 2
+var i = 0;
+while (i < miLista.length) {
+  console.log(miLista[i]);
+  i++;
+}
+
+// Ejercicio 3
+const saludo = () => console.log('Hola mundo');
+saludo();
